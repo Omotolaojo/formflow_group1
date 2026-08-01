35b3430e3432e4a08552c3817e62d3f5acd96962
David <nnamanidavidaws@gmail.com>
2026-08-01 14:08:19 +0100

    terraform fmt on all files

--- Infrastructure/terraform/subnet.tf
+++ Infrastructure/terraform/subnet.tf
@@ -3,5 +3,5 @@ resource "azurerm_subnet" "subnet-formflow" {
   resource_group_name  = azurerm_resource_group.rg-formflow.name
   virtual_network_name = azurerm_virtual_network.vnet-formflow.name
   address_prefixes     = var.subnet_address
-  
+
 }
